76f14538da02584625a17d822e96a941b93be698
Vanessa Retz <vanessa.retz@aol.com>
2018-06-21 15:14:22 +0200

    define Constructors & functions

---
+++ framework/Box.hpp
@@ -0,0 +1,34 @@
+#ifndef BOX_HPP
+#define BOX_HPP
+
+#include <glm/vec3.hpp>
+#include <string>
+#include <iostream>
+
+#include "Shape.hpp"
+#include "color.hpp"
+
+using namespace std;
+
+class Box : public Shape {
+
+    public :
+        Box();
+        Box(glm::vec3 const& min, glm::vec3 const& max);
+        Box(glm::vec3 const& min, glm::vec3 const& max, string const& name, Color const& color);
+
+        glm::vec3 get_min() const;
+        glm::vec3 get_max() const;
+
+        double area() const override;
+        double volume() const override;
+
+        ostream& print(ostream& ost) const override;
+
+    private :
+        glm::vec3 min_;
+        glm::vec3 max_;
+
+};
+
+#endif
